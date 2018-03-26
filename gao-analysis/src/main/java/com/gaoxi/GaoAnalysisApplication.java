02c15ea99e85a3111859a0b05545db903f9c3b58
liweiyuan <535767595@qq.com>
2018-03-26 17:23:50 +0800

    基于springboot的dubbo分布式微服务，运行在docker

---
+++ gao-analysis/src/main/java/com/gaoxi/GaoAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.gaoxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaoAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaoAnalysisApplication.class, args);
+	}
+}
